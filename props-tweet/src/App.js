90d40097b90a836e12213bde48919c27052a5658
over40dev <over40dev.com>
2020-04-16 15:02:24 -0230

    Props Tweet -  completed

--- props-tweet/src/App.js
+++ props-tweet/src/App.js
@@ -1,4 +1,5 @@
 import React, { Component } from "react";
+import moment from "moment";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import {
   faHeart,
@@ -8,66 +9,95 @@ import {
 } from "@fortawesome/free-solid-svg-icons";
 import "./App.css";
 
+const testTweet = {
+  message: "This is less than 140 characters!",
+  gravatar: "nothing",
+  author: {
+    name: "My Name",
+    handle: "@myhandle",
+  },
+  likes: 2,
+  retweets: 10,
+  timestamp: "2020-01-01 01:01:30",
+};
+
 export default class AppComponent extends Component {
   render() {
-    return <Tweet />;
+    return <Tweet tweet={testTweet} />;
   }
 }
 
-function Tweet() {
+function Tweet({ tweet }) {
   return (
     <div className="tweet">
-      <Avatar />
+      <Avatar hash={tweet.gravatar} />
       <div className="content">
-        <NameWithHandle name="My Name" handle="@myhandlde" />
-        <Time hours={10}/>
-        <Message />
+        <NameWithHandle author={tweet.author} />
+        <Time time={tweet.timestamp} />
+        <Message text={tweet.message} />
         <div className="buttons">
-          <LikeButton />
+          <LikeButton count={tweet.likes} />
           <CommentButton />
           <ShareButton />
-          <RetweetButton />
+          <RetweetButton count={tweet.retweets} />
         </div>
       </div>
     </div>
   );
 }
 
-function Avatar() {
-  return (
-    <img
-      className="avatar"
-      src="https://gravatar.com/avatar/nothing"
-      alt="avatar"
-    />
-  );
+function Avatar(props) {
+  const url = `https://gravatar.com/avatar/${props.hash}`;
+  return <img className="avatar" src={url} alt="avatar" />;
 }
 
-function NameWithHandle({name, handle}) {
+function NameWithHandle({ author }) {
+  const { name, handle } = author;
   return (
     <>
       <span className="name">{name}</span>
-      <span className="name">{handle}</span>
+      <span className="handle">{handle}</span>
     </>
   );
 }
 
-function Message() {
-  return <div className="message">This is less than 140 characters!</div>;
-}
+const Message = ({ text }) => <div className="message">{text}</div>;
 
-const Time = (props) => <span className="time">{props.hours}h ago</span>;
+const Time = ({ time }) => {
+  const timeString = moment(time).fromNow();
+  return <span className="time">{timeString}</span>;
+};
 
-const LikeButton = () => <FontAwesomeIcon className="button" icon={faHeart} />;
+const LikeButton = ({ count }) => {
+  return (
+    <span className="like-button">
+      <FontAwesomeIcon icon={faHeart} />
+      {count > 0 && <span className="like-count">{count}</span>}
+    </span>
+  );
+};
 
 const CommentButton = () => (
-  <FontAwesomeIcon className="button" icon={faComment} />
+  <span className="comment-button">
+    <FontAwesomeIcon icon={faComment} />
+  </span>
 );
 
 const ShareButton = () => (
-  <FontAwesomeIcon className="button" icon={faExternalLinkAlt} />
+  <span className="share-button">
+    <FontAwesomeIcon icon={faExternalLinkAlt} />
+  </span>
 );
 
-const RetweetButton = () => (
-  <FontAwesomeIcon className="button" icon={faRetweet} />
+const RetweetButton = ({ count }) => (
+  <span className="retweet-button">
+    <FontAwesomeIcon icon={faRetweet} />
+    {getRetweetCount(count)}
+  </span>
 );
+
+function getRetweetCount(count) {
+  if (count > 0) {
+    return <span className="retweet-count">{count}</span>;
+  }
+}
