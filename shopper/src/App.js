9f007e4b590cb6e4b273e2543fa890a6ac61f0f7
over40dev <over40dev@gmail.com>
2020-04-23 01:15:41 -0230

    za-react-course-project-shopping - Part 2 complete

--- shopper/src/App.js
+++ shopper/src/App.js
@@ -1,16 +1,31 @@
-import React from 'react';
+import React, { useState } from 'react';
 import Nav from './Nav';
 import './App.css';
 
 const App = () => {
+  const [activeTab, setActiveTab] = useState('items');
+
   return (
     <div className="App">
-      <Nav/>
+      <Nav activeTab={activeTab} onTabChange={setActiveTab} />
       <main className="App-content">
-        <span>Empty</span>
+        <span>
+          <Content tab={activeTab} />
+        </span>
       </main>
     </div>
-  )
-}
+  );
+};
+
+const Content = ({ tab }) => {
+  switch (tab) {
+    case 'items':
+      return <span>the Items</span>;
+    case 'cart':
+      return <span>the Cart</span>;
+    default:
+      return <span>No Content</span>;
+  }
+};
 
 export default App;
--- shopper/src/Nav.js
+++ shopper/src/Nav.js
@@ -1,18 +1,23 @@
 import React from 'react';
 
-const Nav = () => {
+const Nav = ({ activeTab, onTabChange }) => {
+  const itemClass = (tabName) =>
+  `
+  App-nav-item ${activeTab === tabName ? 'selected' : ''}
+  `;
+
   return (
     <nav className="App-nav">
       <ul>
-        <li className="App-nav-item">
-          <a>Items</a>
+        <li className={itemClass('items')}>
+          <a onClick={() => onTabChange('items')}>Items</a>
         </li>
-        <li className="App-nav-item">
-          <a>Cart</a>
+        <li className={itemClass('cart')}>
+          <a onClick={() => onTabChange('cart')}>Cart</a>
         </li>
       </ul>
     </nav>
   );
-}
+};
 
 export default Nav;
