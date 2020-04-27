139cf4b625e4735e62a5d3bea63ffd591fd88e87
over40dev <over40dev@gmail.com>
2020-04-27 00:59:19 -0230

    za-react-course-project-shopping - Part 5 WIP-2

--- shopper/src/App.js
+++ shopper/src/App.js
@@ -1,82 +1,92 @@
-import React, { useState } from 'react';
+import React from 'react';
 import Nav from './Nav';
-import ItemPage from './ItemPage';
-import CartPage from './CartPage';
-import { items } from './static-data';
+// import ItemPage from './ItemPage';
+// import CartPage from './CartPage';
+// import { items } from './static-data';
 import './App.css';
 
 class App extends React.Component {
-
   state = {
     activeTab: 0,
-    cart: [],
-  }
+    cart: {},
+  };
 
-  handleTabChange = (index) => {
+  tabChange = (index) => {
     this.setState({
       activeTab: index,
     });
-  }
-  
-  handleAddToCart = (item) => {
+  };
+
+  addToCart = (item) => {
+    // 1. take copy of initial state
+    const cart = { ...this.state.cart };
+    // 2. if item exists in cart then increase count by 1;
+    //    otherwise add item and initialize count to 1
+    cart[item.id] = cart[item.id] + 1 || 1;
+    // 3. use React State API to update state
     this.setState({
-      cart: [...this.state.cart, item.id],
+      cart,
     });
-  }
-  
-  handleRemoveOne = (item) => {
-    const index = this.state.cart.indexOf(item.id);
+  };
+
+  removeFromCart = (item) => {
+    // 1. take copy of initial state
+    const cart = { ...this.state.cart };
+    // 2. if item exists in cart then decrease count by 1;
+    //    if count gets to 0 then remove from cart
+    cart[item.id] = cart[item.id] - 1 || 0;
+    if (cart[item.id] <= 0) {
+      delete cart[item.id];
+    }
+    // 3. use React State API to update state
     this.setState({
-      ...this.state.cart.slice(0, index),
-      ...this.state.cart.slice(index + 1),
+      cart,
     });
-  }
-  
+  };
+
   render() {
-    <div className="App">
-    <Nav activeTab={activeTab} onTabChange={this.handleTabChange} />
-    <main className="App-content">
-      <span>
-        <Content tab={activeTab} onAddToCart={this.handleAddToCart} />
-      </span>
-    </main>
-  </div>
+    return (
+      <div className="App">
+        <Nav activeTab={this.state.activeTab} tabChange={this.tabChange} />
+        <main className="App-content">
+          <span>render Content here!</span>
+        </main>
+      </div>
+    );
   }
 }
 
-const Content = ({ tab, handleAddToCart }) => {
-  switch (tab) {
-    case 'items':
-      return (
-        <>
-          <h2>Items</h2>
-          <ItemPage items={items} onAddToCart={handleAddToCart} />
-        </>
-      );
-    case 'cart':
-      return (
-        <>
-          <h2>Cart</h2>
-          {/* <CartPage items={cart} /> */}
-        </>
-      );
-    default:
-      return <h2>No Content</h2>;
-  }
-};
+// const Content = ({ tab, addToCart }) => {
+//   switch (tab) {
+//     case 0:
+//       return (
+//         <>
+//           <h2>Items</h2>
+//           <ItemPage items={items} addToCart={addToCart} />
+//         </>
+//       );
+//     case 1:
+//       return (
+//         <>
+//           <h2>Cart</h2>
+//         </>
+//       );
+//     default:
+//       return <h2>No Content</h2>;
+//   }
+// };
 
-const summarizeCart = () => {
-  const groupedItems = cart.reduce((summary, item) => {
-    summary[item.id] = summary[item.id] || {
-      ...item,
-      count: 0
-    };
-    summary[item.id].count++;
-    return summary;
-  }, {});
+// const summarizeCart = (cart) => {
+//   const groupedItems = cart.reduce((summary, item) => {
+//     summary[item.id] = summary[item.id] || {
+//       ...item,
+//       count: 0,
+//     };
+//     summary[item.id].count++;
+//     return summary;
+//   }, {});
 
-  return Object.values(groupedItems);
-
-}
+//   return Object.values(groupedItems);
+// };
 
 export default App;
--- shopper/src/Nav.js
+++ shopper/src/Nav.js
@@ -1,19 +1,19 @@
 import React from 'react';
 
-const Nav = ({ activeTab, onTabChange }) => {
-  const itemClass = (tabName) =>
+const Nav = ({ activeTab, tabChange }) => {
+  const itemClass = (tabNum) =>
   `
-  App-nav-item ${activeTab === tabName ? 'selected' : ''}
+  App-nav-item ${activeTab === tabNum ? 'selected' : ''}
   `;
 
   return (
     <nav className="App-nav">
       <ul>
-        <li className={itemClass('items')}>
-          <a onClick={() => onTabChange('items')}>Items</a>
+        <li className={itemClass(0)}>
+          <a onClick={() => tabChange(0)}>Items</a>
         </li>
-        <li className={itemClass('cart')}>
-          <a onClick={() => onTabChange('cart')}>Cart</a>
+        <li className={itemClass(1)}>
+          <a onClick={() => tabChange(1)}>Cart</a>
         </li>
       </ul>
     </nav>
